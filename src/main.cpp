710afad9f1dc6b5321b9a6331bb5c80e4bbea0bc
Hans Sebastian <hans.sebastian.work@gmail.com>
2024-12-18 17:37:43 +0700

    Added initial wifi code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <esp_wifi.h>
+#include <esp_tls_errors.h>
 
 #define SENSOR_PIN 0 // Change to actual pin number
 
 uint32_t count = 0;
+wifi_ap_config_t ap_conf = {};
+wifi_sta_config_t sta_conf = {};
 
 const inline uint32_t read_sensor() noexcept {
     const uint32_t register timeout_cycles = microsecondsToClockCycles(10000000);
@@ -29,9 +33,42 @@ const inline uint32_t read_sensor() noexcept {
 }
 
 void setup() {
-    // put your setup code here, to run once:
+    wifi_init_config_t init_conf = WIFI_INIT_CONFIG_DEFAULT();
+    esp_err_t retcode;
+    if ((retcode = esp_wifi_init(&init_conf)) != ESP_OK) {
+        Serial.print("Failed to initialize wifi with return code: ");
+        Serial.println(retcode);
+    }
+    if ((retcode = esp_wifi_set_mode(wifi_mode_t::WIFI_MODE_APSTA)) != ESP_OK) {
+        Serial.print("Failed to set wifi mode with return code: ");
+        Serial.println(retcode);
+    }
+    wifi_config_t conf = {};
+    if ((retcode = esp_wifi_get_config(wifi_interface_t::WIFI_IF_AP, &conf))) {
+        Serial.print("Failed to get wifi AP config with return code: ");
+        Serial.println(retcode);
+    }
+    ap_conf = conf.ap;
+    conf = {};
+    if ((retcode = esp_wifi_get_config(wifi_interface_t::WIFI_IF_STA, &conf))) {
+        Serial.print("Failed to get wifi STA config with return code: ");
+        Serial.println(retcode);
+    }
+    sta_conf = conf.sta;
+    if ((retcode = esp_wifi_start()) != ESP_OK) {
+        Serial.print("Failed to start wifi with return code: ");
+        Serial.println(retcode);
+    }
+    if ((retcode = esp_wifi_connect()) != ESP_OK) {
+        Serial.print("Failed to connect to ");
+        // Serial.print(ap_conf.);
+        Serial.print(" with retcode: ");
+        Serial.println(retcode);
+    }
+
+    // Set sensor pin mode to input
     pinMode(SENSOR_PIN, INPUT);
-    
+
 }
 
 
@@ -43,6 +80,5 @@ void loop() {
         count++;
     }
 
-    // put your main code here, to run repeatedly:
 }
 
